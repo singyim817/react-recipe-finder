f5cc9872bcf1d1990453cf579f336c0226024fd0
Sing YIM <yks902@ha.org.hk>
2021-04-14 10:54:51 +0800

    init

---
+++ src/components/ResearchRecipes.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { Form, FormGroup, FormControl, Button } from 'react-bootstrap'
+import { connect } from 'react-redux';
+import { setRecipes } from '../actions';
+
+class SearchRecipes extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            ingredients: '',
+            dish: '',
+        }
+    }
+
+    search() {
+        let { ingredients, dish } = this.state;
+        const url = `http://www.recipepuppy.com/api/?i=${ingredients}&q=${dish}`;
+        console.log('state', this.state, 'url', url);
+
+        fetch(url, {
+            method: 'GET'
+        }).then(response => response.json())
+        .then(json => {
+            console.log('recipes', json);
+            this.props.setRecipes(json.results);
+        });
+    }
+
+    render() {
+        return (
+            <Form inline>
+                <FormGroup>
+                    <Form.Label>Ingredients</Form.Label>
+                    {' '}
+                    <FormControl
+                        type="text"
+                        placeholder="garlic, chicken"
+                        onChange={event => this.setState({ ingredients: event.target.value })}
+                    />
+                </FormGroup>
+                {' '}
+                <FormGroup>
+                    <Form.Label>Dish</Form.Label>
+                    {' '}
+                    <FormControl
+                        type="text"
+                        placeholder="adobo"
+                        onChange={event => this.setState({ dish: event.target.value })}
+                    />
+                </FormGroup>
+                {' '}
+                <Button onClick={() => this.search()}>Submit</Button>
+            </Form>
+        )
+    }
+}
+
+export default connect(null, { setRecipes })(SearchRecipes);
--- src/index.js
+++ src/index.js
@@ -2,11 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import FavouriteRecipeList from './components/FavouriteRecipeList';
+
+const store = createStore(rootReducer);
+store.subscribe(() => {
+  console.log('store', store.getState());
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={App}></Route>
+          <Route path='/favourites' component={FavouriteRecipeList}></Route>
+        </Switch>
+      </BrowserRouter>
+      {/* <App /> */}
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
